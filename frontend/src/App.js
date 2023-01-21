abac87c28cbea2ebc730ba0605a18c7686dfde05
Vivian Li <viviannnlyr23@gmail.com>
2023-01-21 14:43:56 -0500

    user authentication done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 import Header from './components/Header'
 
@@ -20,6 +22,7 @@ function App() {
           </Routes>
         </div>
       </Router>
+      <ToastContainer />
     </>
   );
 }
